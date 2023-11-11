3d6b6f3d1beba59d4994857eed45e62edfe119a7
Paras <paras.b1ank@gmail.com>
2023-11-11 14:47:44 +0530

    Added Music commands

--- dep-installer-hack/build.rs
+++ dep-installer-hack/build.rs
@@ -5,38 +5,36 @@ fn main() {
         .unwrap_or_default()
         .contains("shuttle")
     {
-        if !std::process::Command::new("apt")
-            .arg("install")
-            .arg("-y")
-            .arg("software-properties-common").status().expect("Failed to run apt").success() {
 
-            panic!("Failed to install apt tools");
-        }
-
-        if !std::process::Command::new("add-apt-repository")
-            .arg("ppa:tomtomtom/yt-dlp").status().expect("Failed to run add-apt-repository").success() {
-            
-            panic!("Failed to add repo");
-        }
-
-        if !std::process::Command::new("apt")
-            .arg("update").status().expect("Failed to update package list").success() {
-
-            panic!("Failed to Update package list");
-        }
 
         if !std::process::Command::new("apt")
             .arg("install")
             .arg("-y")
             .arg("libopus-dev") // the apt package that a dependency of my project needs to compile
             .arg("ffmpeg")
-            .arg("yt-dlp")
+            .arg("curl")
             // can add more here
             .status()
             .expect("failed to run apt")
             .success()
         {
             panic!("failed to install dependencies")
+        }
+
+        if !std::process::Command::new("curl")
+            .arg("-L")
+            .arg("https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp")
+            .arg("-o")
+            .arg("/usr/local/bin/yt-dlp").status().expect("Failed to run curl").success() {
+
+            panic!("Failed to download YT-dlp");
+        }
+
+        if !std::process::Command::new("chmod")
+            .arg("a+rx")
+            .arg("/usr/local/bin/yt-dlp").status().expect("Failed to run chmod").success() {
+
+            panic!("Failed to make yt-dlp executable");
         }
     }
 }
